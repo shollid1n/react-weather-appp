0806dc0b259260068b3120682b452824d524e8ed
shollid1n <shollid1n@gmail.com>
2022-12-18 19:52:46 -0600

    Update App.js

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ export default function App() {
      <h1> Weather App</h1>
     <footer>
      This project was coded by <a href="https://shalinda-portfolio.netlify.app/" target="_blank"> Shalinda Holliday </a> and is {""}
-      <a href='https://github.com/shollid1n/react-weather-appp'
+      <a href="https://github.com/shollid1n/react-weather-appp"
       target="_blank"
       >open-sourced on GitHub</a>
       </footer>
