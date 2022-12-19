0e881c9d41f28659d75dc755e20e3451ff14fab6
shollid1n <shollid1n@gmail.com>
2022-12-18 19:39:04 -0600

    Added the Github repo url

--- src/App.js
+++ src/App.js
@@ -1,12 +1,18 @@
 
 import './App.css';
 
-function App() {
+export default function App() {
   return (
     <div className="App">
-      Weather App
+     <h1> Weather App</h1>
+    <footer>
+     This project was coded by <a href="https://shalinda-portfolio.netlify.app/" target="_blank"> Shalinda Holliday </a> and is {""}
+      <a href='https://github.com/shollid1n/react-weather-appp'
+      target="_blank"
+      >open-sourced on GitHub</a>
+      </footer>
     </div>
   );
 }
 
-export default App;
+
