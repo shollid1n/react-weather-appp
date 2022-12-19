6b47996ee05f20b21a0375a29f734dc003e2b2a3
shollid1n <shollid1n@gmail.com>
2022-12-18 21:12:32 -0600

    Added html structure

--- src/App.js
+++ src/App.js
@@ -1,12 +1,12 @@
-
+import Weather from "./Weather";
 import './App.css';
 
 export default function App() {
   return (
     <div className="App">
       <div className="container">
-     <h1> Weather App</h1>
-     
+   
+     <Weather />
     <footer>
      This project was coded by <a href="https://shalinda-portfolio.netlify.app/" target="_blank noreferrer"> 
      Shalinda Holliday 
