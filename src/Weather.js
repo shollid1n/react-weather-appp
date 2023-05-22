488f07d87fe59780ca1b7a2ef97456033ac75ef8
shollid1n <shollid1n@gmail.com>
2023-05-21 21:18:56 -0500

    metric

--- src/Weather.js
+++ src/Weather.js
@@ -3,6 +3,7 @@ import WeatherInfo from "./WeatherInfo";
 import axios from "axios";
 import "./Weather.css";
 
+
 export default function Weather(props) {
     
      const [weatherData, setWeatherData] = useState({ready: false});
@@ -25,7 +26,7 @@ export default function Weather(props) {
     function search() {
         const apiKey = "fbef01f4et1b02o0d25c27210a43ef3f";
         let apiUrl = 
-    `https://api.shecodes.io/weather/v1/current?query=${city}&key=${apiKey}&units=imperial`;
+    `https://api.shecodes.io/weather/v1/current?query=${city}&key=${apiKey}&units=metric`;
     axios.get(apiUrl).then(handleResponse);
     }
 
