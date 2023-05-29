e58f155278313971b2cd50617cc50ab01bdb7d6b
shollid1n <shollid1n@gmail.com>
2023-05-28 20:48:08 -0500

    Update WeatherIcon.js

--- src/WeatherIcon.js
+++ src/WeatherIcon.js
@@ -4,7 +4,7 @@ import React from "react";
 
 export default function WeatherIcon(props) {
     console.log(props.code);
-    return (
+     return (
       
     const codeMapping = {
         "clear-sky-day": "Clear sky",
