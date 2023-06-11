85bbd02a38fb192f76868bab894b52561dc76758
shollid1n <shollid1n@gmail.com>
2023-06-11 17:11:10 -0500

    weather forecast icon

--- src/WeatherForecast.js
+++ src/WeatherForecast.js
@@ -6,10 +6,13 @@ export default function WeatherForecast() {
     return (<div className="WeatherForecast">
           <div className="row">
             <div className="col">
-               <div className="WeatherForecast-day">Thur</div> 
+               <div className="WeatherForecast-day">Thur
+               
+               </div> 
+               
               <div className="WeatherForecast-temperatures">
                 <span className="WeatherForecast-temperature-max">19°</span>
-                <span className="WeatherForecast-temperature-min">19</span>
+                <span className="WeatherForecast-temperature-min">10</span>
                 </div>
             </div>
           </div>
--- src/WeatherInfo.js
+++ src/WeatherInfo.js
@@ -17,7 +17,7 @@ export default function WeatherInfo(props) {
         <div className="col-6">
           <div className="clearfix">
             <div className="float-left">
-          <img src={props.data.icon} alt="weather-image" />
+          <img src={props.data.icon}  alt="weather-image" />
 
               <div className="float-left">
                 <WeatherTemperature celsius={props.data.temperature} />
